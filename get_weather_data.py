ab78073fe652fb9f761fcccbf850e5127c1e950f
AdamOlsson <Olsson.95_@hotmail.com>
2021-10-02 19:27:21 +0200

    Initial commit

---
+++ get_weather_data.py
@@ -0,0 +1,40 @@
+import requests, json, calendar, datetime
+
+config_path = "config.json"
+weather_transation_path = "wsymb2.json"
+
+with open(config_path, 'r') as f:
+    config = json.load(f)
+
+with open(weather_transation_path, 'r') as f:
+    weather_transation = json.load(f)
+
+smhi_url = config["smhi_url"]
+lat, lon = config["lat"], config["lon"]
+smhi_endpoint = config["smhi_endpoint"].format(lon, lat)
+
+r = requests.get(smhi_url + smhi_endpoint)
+
+response_json = json.loads(r.text)
+response_json["statusCode"] = r.status_code
+response_json["timeSeries"] = response_json["timeSeries"][:18] # Keep next 24h
+
+for timestamp in response_json["timeSeries"]:
+    date, time = timestamp["validTime"].split("T")  # from "2021-10-02T16:00:00Z" to "2021-10-02" and "16:00:00Z"
+    time = "{}:{}".format(*time.split(":")[0:2]) # from "16:00:00Z" to "16:00"
+    timestamp["time"] = time
+    timestamp["date"] = date
+    
+    y, m, d = date.split("-")
+    date = datetime.date(int(y), int(m), int(d))
+    timestamp["dayName"] = calendar.day_name[date.weekday()]
+
+    for param in timestamp["parameters"]:
+        if param["name"] == "Wsymb2":
+            weather_code = int(param["values"][0])
+            timestamp["weather"] = weather_transation[str(weather_code)]
+            timestamp["weather_code"] = weather_code
+            break
+
+with open("data.json", 'w') as f:
+    json.dump(response_json, f)
---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+const HTTP_GET = "GET"
+ 
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    switch (req.method) {
+        case HTTP_GET:
+            handleGet(req, res);
+            break;
+        default:
+            console.log("Recevied bad request.");
+            res.statusCode = 501;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Not yet implemented.\n');        
+            break
+    }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+function handleGet(req, res) {
+    let rawdata = fs.readFileSync('data.json');
+    let weatherdata = JSON.parse(rawdata);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.end(JSON.stringify(weatherdata))
+    
+}
